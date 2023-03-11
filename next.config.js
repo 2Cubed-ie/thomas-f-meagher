25ede353b8e712670689169fc274b1d0367c6e49
Artem66 <melodpi@gmail.com>
2023-03-11 08:16:19 +0000

    added PWA module

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development'
+  }
 };
 
 const withPWA = require('next-pwa')({
