940ce354957afee9d4d623ca5dde716195e2a2fb
Artem66 <melodpi@gmail.com>
2023-03-11 08:52:54 +0000

    fixed warn issues

--- next.config.js
+++ next.config.js
@@ -1,16 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: true
+};
+
+const withPWA = require('next-pwa')({
   pwa: {
     dest: "public",
     register: true,
     skipWaiting: true,
     disable: process.env.NODE_ENV === 'development'
   }
-};
-
-const withPWA = require('next-pwa')({
-  dest: 'public',
 });
 
 module.exports = withPWA(nextConfig);
